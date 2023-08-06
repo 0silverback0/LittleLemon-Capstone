c8d95b3ef4b7839a8f38ecfccf552e96e141aff4
SilverBack Gorilla <silverbackgorilla@SilverBacks-MacBook-Pro.local>
2023-08-06 01:09:04 -0400

    added models

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    ID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    no_of_guests = models.PositiveIntegerField()
+    BookingDATE = models.DateField()
+
+class Menu(models.Model):
+    ID = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory = models.PositiveIntegerField()
